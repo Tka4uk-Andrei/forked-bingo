68934f9a634a25a350801c1d923b632204c4af86
Tkachuk Andrey <tka4uk.andrei@yandex.ru>
2019-11-13 00:05:16 +0300

    Updates

---
+++ precache-manifest.1fc73245e690dab36ac574b05e334175.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159e72e45aa5fda5bcaab20312973c69",
+    "url": "/forked-bingo/index.html"
+  },
+  {
+    "revision": "a582948fb7b6c3cb69c3",
+    "url": "/forked-bingo/static/css/main.5dd601ed.chunk.css"
+  },
+  {
+    "revision": "4edc7d63872b42632425",
+    "url": "/forked-bingo/static/js/2.14e6b7b6.chunk.js"
+  },
+  {
+    "revision": "a582948fb7b6c3cb69c3",
+    "url": "/forked-bingo/static/js/main.fe1e75a3.chunk.js"
+  },
+  {
+    "revision": "55c1f9becc04ed6d7670",
+    "url": "/forked-bingo/static/js/runtime-main.e76db7d3.js"
+  }
+]);
